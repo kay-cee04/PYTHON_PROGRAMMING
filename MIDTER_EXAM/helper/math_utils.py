8276ff0765c72cb8b6bedba595d49c463da56196
Kathleen <daclankath.23@gmail.com>
2025-10-23 11:21:16 +0800

    Add files via upload

---
+++ MIDTER_EXAM/helper/math_utils.py
@@ -0,0 +1,44 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "cc32ffad-3388-4859-a779-66d63df7e67c",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "def area(length, width):\n",
+    "    return length * width\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "69da5275-e7e1-4982-95a0-95d9e5e29c29",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.13.5"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
